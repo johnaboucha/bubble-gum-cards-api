f8ead531915c72d66b77d806e7d4bef1ffb0f636
John Boucha <johnaboucha@users.noreply.github.com>
2022-10-18 22:43:39 -0500

    cards added; categories fixed

--- routers/categories.py
+++ routers/categories.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from security.keys import api_key_auth, keys
-from data.data import categories
+from data.sheets import categories
 
 router = APIRouter(prefix="/api/categories")
 
